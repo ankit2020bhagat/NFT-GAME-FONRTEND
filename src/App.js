fa210ead85253136824eb536fbcadfb3206bf0e9
Ankit kumar <animusk7765@gmail.com>
2023-07-19 22:54:21 +0530

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import SelectCharacter from "./Components/SelectCharacter";
-import twitterLogo from "./assets/twitter-logo.svg";
 import Arena from "./Components/Arena";
 import LoadingIndicator from "./Components/LoadingIndicator";
 import { ethers } from "ethers";
